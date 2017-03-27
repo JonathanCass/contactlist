4378c461742556d84e129752983f4babbbd98112
JonCass <JCass1980@gmail.com>
2017-03-27 15:41:16 -0700

    links not working

---
+++ src/SingleView.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Link, Route} from 'react-router-dom'
+
+export default React.createClass({
+  render(){
+  	return (
+  		<div>
+  			<p>SingleView</p>
+  		</div>
+  	)
+  }
+})
