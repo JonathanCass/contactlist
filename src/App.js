435797ec782e2b0e21dec88a5dcaea15937a64e6
JonCass <JCass1980@gmail.com>
2017-03-27 17:00:12 -0700

    a

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from 'react'
 import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
-import data from './data'
 import shortid from 'shortid'
 import SingleView from './SingleView'
 import ListView from './ListView'
 
 export default React.createClass({
-  getInitialState: function (){
-    return {
-      id: 1
-    }
-  },
   render(){
     return(
       <Router>
-        <ListView/ >
+        <div>
+          <Link to="/ListView">ListView</Link>
+        
+          <Route path='/ListView' component={ListView} />
+        </div>
       </Router>
     )
   }
